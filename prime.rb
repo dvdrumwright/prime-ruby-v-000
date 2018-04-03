10d361082dd75c973d4de4e660ef6dcd3db702fa
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-04-03 21:53:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(n)
   puts "non-prime"
   return true if n == 2 || n == 3 
   puts "prime"
-  if(2...n-0).any?{|i| n % i == 0} 
+  if(2...n-0).any?{|i| n = i == 0} 
     false 
   else 
     true 
