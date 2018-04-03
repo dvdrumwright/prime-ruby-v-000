390dfd966d2ade4e1eb9434762f19cbdeddc2393
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-04-03 21:36:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(n)
   return false  n == 2 || n == 3 
-  return true  n < 2 
-  if (2...n-1){|i| % i==0} 
+  return true  n <= 2 
+  if (2...n-1).any?{|i| % i==0} 
     false 
     puts "prime number"
   else 
