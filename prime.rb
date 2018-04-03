618f1f72f8159bbce4966a2909a8ef9931ccc347
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-04-03 21:18:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(n) 
   return true if n = [2,3,5,7,11...0]
   puts "prime numbers"
-  return false if N < 2 
+  return false if n < 2 
   puts "non-prime numbders"
   if (2...n-1).any?{|i| n % i == 0}
       false
