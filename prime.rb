3330d0f842dcaf04755f67518ced98d8e1507b9c
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-04-03 21:35:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,4 +2,10 @@ def prime?(n)
   return true n == 2 || n == 3 
   return false n <= 2 
   if (2...n-1){|i| % i==0} 
-    
+    false 
+    puts "prime number"
+  else 
+    truen 
+    puts "non-prime number"
+  end 
+end 
